89f44b4ec4a967875b73c7a3730309505070ac3c
Tyler Bahen <tylerbahen@gmail.com>
2026-04-21 13:45:53 -0600

    Started on loading functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//set up service worker
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js")
     .then(() => {
@@ -7,6 +8,7 @@ if ("serviceWorker" in navigator) {
 
 }
 
+//Grab when the page changes hash url and handle it
 window.addEventListener('hashchange',() => {
   const page = window.location.hash.replace('#','')
   console.log(page)
@@ -24,6 +26,7 @@ window.addEventListener('hashchange',() => {
   }
 })
 
+//Window changing thingy
 function changeview(window){
   document.getElementById('title').innerHTML = window
   const windows = ['Goals', 'Planner', 'Home', 'People', 'Settings']
@@ -33,6 +36,7 @@ function changeview(window){
     document.getElementById(window).style.visibility = 'visible'
 }
 
+//Emit to the service worker
 function emit(action,messageRaw){
   const message = messageRaw
   message.action = action
@@ -41,4 +45,21 @@ function emit(action,messageRaw){
     });
 }
 
-changeview('Home')
+//pull all the contacts and put them on the page
+function refreshcontacts(){
+  const contactsraw = localStorage.getItem('contacts')
+  if (contactsraw==null){
+    const contacts = []
+  } else {
+    const contacts = JSON.parse(contactsraw.list)
+  }
+  contacts.forEach(contact => {
+    console.log(contact)
+  })
+}
+
+//Onload function
+function load(){
+  refreshcontacts()
+  changeview('Home')
+}
