3401975fa185b71368f4374472faae0376ca27c7
Tyler Bahen <tylerbahen@gmail.com>
2026-04-27 16:09:07 -0600

    More calendar stuff

--- script.js
+++ script.js
@@ -289,7 +289,15 @@ function load(){
   callback: (response) => {
     const user = parseJwt(response.credential)
     document.getElementById('google-signin').remove()
-    document.getElementById('google-wrapper').innerHTML = 'Signed In!'
+    document.getElementById('google-wrapper').innerHTML = `Signed In As ${user.name}`
+    const tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: GOOGLE_CLIENT_ID,
+      scope: "https://www.googleapis.com/auth/calendar.readonly",
+      callback: (response) => {
+        const accessToken = response.access_token;
+        console.log("Access token:", accessToken);
+      }
+    });
   },
 });
   google.accounts.id.renderButton(
