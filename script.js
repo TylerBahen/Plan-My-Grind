34b2dddf7aaabc18078bc34bcce4377be8e0c885
Tyler Bahen <tylerbahen@gmail.com>
2026-04-28 18:17:10 -0600

    Typo fix

--- script.js
+++ script.js
@@ -331,7 +331,7 @@ async function syncGoogleCalendar(){
     } else {
       events = JSON.parse(events)
     }
-    for (const addition of discrepancies.add) {
+    for (const addition of descrepancies.add) {
       const eventDate = eventsRaw[addition]
       const event = events[eventDate].find(o => o.id == addition)
       //POST https://www.googleapis.com/calendar/v3/calendars/primary/events?eventId=UUID@PlanMyGrind
